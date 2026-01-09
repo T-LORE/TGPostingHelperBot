b3476d4f3727a5e39a4f4689ae1174242f6e4ea1
T-LORE <tlore007@gmail.com>
2026-01-10 01:59:31 +0300

    feat: added handlers for video photo and gifs (with album support)

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -1,11 +1,41 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters.command import Command
-from bot.filters.admin import IsAdmin 
+from bot.filters.admin import IsAdmin
+from bot.middlewares.album import AlbumMiddleware  
 
 router = Router()
 router.message.filter(IsAdmin())
+router.message.middleware(AlbumMiddleware(latency=0.5))
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
     await message.answer("Hello world, Admin!")
+
+@router.message(F.photo | F.video | F.animation)
+async def handle_media_content(message: Message, album: list[Message] = None):
+    files_to_process = album if album else [message]
+    added_count = 0
+    
+    for msg in files_to_process:
+        file_id = None
+        media_type = 'photo'
+        
+        if msg.photo:
+            file_id = msg.photo[-1].file_id
+            media_type = 'photo'
+        elif msg.video:
+            file_id = msg.video.file_id
+            media_type = 'video'
+        elif msg.animation:
+            file_id = msg.animation.file_id
+            media_type = 'animation'
+            
+        if file_id:
+            added_count += 1
+
+    await message.reply(f"✅ Добавлено {added_count} медиафайлов в очередь!")
+
+@router.message()
+async def echo_gif(message: Message):
+    await message.reply("Сообщение не обработано!")
---
+++ bot/middlewares/album.py
@@ -0,0 +1,41 @@
+import asyncio
+from typing import Any, Dict, Union, List, Callable, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+
+
+class AlbumMiddleware(BaseMiddleware):
+    def __init__(self, latency: Union[int, float] = 0.5):
+        self.latency = latency
+        self.album_data = {}
+
+    async def __call__(
+        self,
+        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: Dict[str, Any],
+    ) -> Any:       
+        if not event.media_group_id:
+            return await handler(event, data)
+
+        group_id = event.media_group_id
+        
+        if group_id not in self.album_data:
+            print("Album detected!")
+            self.album_data[group_id] = []
+            self.album_data[group_id].append(event)
+            
+            await asyncio.sleep(self.latency)
+
+            my_album: List[Message] = self.album_data[group_id]
+            my_album.sort(key=lambda x: x.message_id)
+
+            data["album"] = my_album
+            
+            del self.album_data[group_id]
+            return await handler(event, data)
+
+        else:
+            self.album_data[group_id].append(event)
+            return
