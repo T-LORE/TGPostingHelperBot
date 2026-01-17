4c526b16d8fbd20828dd38e8360d9591a69e55d4
T-LORE <tlore007@gmail.com>
2026-01-18 01:50:17 +0300

    feat: added alert to not handled callback

--- bot/handlers/admin/routers/unknown.py
+++ bot/handlers/admin/routers/unknown.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 import bot.windows.admin as window
 
@@ -10,3 +10,9 @@ async def unknown_command(message: Message):
     message_text, reply_markup = await window.get_unknown_command_window()
     await message.reply(message_text,
     reply_markup=reply_markup)
+
+
+
+@router.callback_query()
+async def unknown_callback(callback: CallbackQuery):
+    await callback.answer("Эта кнопка больше не активна или действие неизвестно. Возможно старое сообщение или бот был перезапущен.", show_alert=True)
